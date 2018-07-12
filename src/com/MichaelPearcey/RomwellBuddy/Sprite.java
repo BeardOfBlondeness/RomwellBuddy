5bb7b43785d4a7c6b07ffd5d74e94c171749cbb0
Pear <mpearcey.59024@farnborough.ac.uk>
2018-07-12 21:21:32 +0100

    Cleaned up some code

--- src/com/MichaelPearcey/RomwellBuddy/Sprite.java
+++ src/com/MichaelPearcey/RomwellBuddy/Sprite.java
@@ -4,7 +4,6 @@ package com.MichaelPearcey.RomwellBuddy;
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glColor3f;
-import static org.lwjgl.opengl.GL11.glColor4f;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
 import static org.lwjgl.opengl.GL11.glPushMatrix;
@@ -14,7 +13,6 @@ import static org.lwjgl.opengl.GL11.glVertex2f;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
-
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,10 +23,6 @@ public class Sprite {
     protected int yPos;
     protected float xRes;
     protected float yRes;
-    protected  float RATIO = 1;
-    protected float alpha = 0;
-    protected boolean fadeIn = true;
-    protected static boolean fadeFinish = false;
     protected Texture tex;
 
     public Sprite(String path, int xPos, int yPos) {
@@ -39,20 +33,6 @@ public class Sprite {
         xRes = tex.getImageWidth();
         yRes = tex.getImageHeight();
     }
-    public static void setFadeFinish(boolean a) {
-        fadeFinish = a;
-    }
-    public float getRatio() {
-        return RATIO;
-    }
-
-    public void setRatio(float ratio) {
-        RATIO = ratio;
-    }
-
-    public String getPath() {
-        return path;
-    }
 
     public void setxRes(float xRes) {
         this.xRes = xRes;
@@ -62,36 +42,12 @@ public class Sprite {
         this.yRes = yRes;
     }
 
-    public int getxPos() {
-        return xPos;
-    }
-
     public void setxPos(int xPos) {
         this.xPos = xPos;
     }
 
-    public int getyPos() {
-        return yPos;
-    }
-
     public void setyPos(int yPos) {
         this.yPos = yPos;
-    }
-
-    public float getxRes() {
-        return xRes;
-    }
-
-    public void setxRes(int xRes) {
-        this.xRes = xRes;
-    }
-
-    public float getyRes() {
-        return yRes;
-    }
-
-    public void setyRes(int yRes) {
-        this.yRes = yRes;
     }
 
     public void draw() {
@@ -111,46 +67,7 @@ public class Sprite {
         glPopMatrix();
     }
 
-
-    public void drawFade() {
-        glPushMatrix();
-        tex.bind();
-        glTranslatef(xPos, yPos, 1);
-        glColor4f(1,2,3,alpha);
-        glBegin(GL_QUADS);
-        {
-            glTexCoord2f(0, 0);
-            glVertex2f(0, 0);
-            glTexCoord2f(0, tex.getHeight());
-            glVertex2f(0, yRes);
-            glTexCoord2f(tex.getWidth(), tex.getHeight());
-            glVertex2f(xRes,yRes);
-            glTexCoord2f(tex.getWidth(), 0);
-            glVertex2f(xRes,0);
-        }
-        glEnd();
-        glPopMatrix();
-        doubleFadeAlpha();
-    }
-
-    public void doubleFadeAlpha() {
-        if(!fadeFinish) {
-            if(alpha <= 1.2 && fadeIn) {
-                alpha+=0.005;
-            }else if(alpha >= -0.05){
-                fadeIn = false;
-                alpha-=0.005;
-            } else {
-                fadeFinish = true;
-            }
-        }
-    }
-
-    protected boolean checkFadeStop() {
-        return fadeFinish;
-    }
-
-    protected Texture loadTexture() {
+    private Texture loadTexture() {
         String fileType = path.substring(path.length() - 3);
         if(fileType.equals("peg"))
             fileType = "jpeg";
